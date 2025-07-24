d0d7d6299090213b1561fab52eeae8f68e9de940
Phan Khai <khaiphan@Daemon.local>
2025-07-24 17:02:27 +0700

    refactor to separate esClient and general es action

--- internal/elasticsearch/elasticsearch.go
+++ internal/elasticsearch/elasticsearch.go
@@ -5,36 +5,15 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/elastic/go-elasticsearch/v9"
    "github.com/elastic/go-elasticsearch/v9/esapi"
 
 	"github.com/khaiphan29/logpulse/pkg/logger"
 )
 
-var es *elasticsearch.Client
-
-func init() {
-   if es != nil {
-      return
-   }
-	var err error
-	es, err = elasticsearch.NewClient(elasticsearch.Config{
-		Addresses: []string{
-			"http://localhost:9200", // Replace with your custom address and port
-		},
-	})
-	if err != nil {
-      logger.Fatal("Error creating Elasticsearch client", map[string]any{
-         "error": err,
-      })
-	} else {
-      logger.Info("Elasticsearch client created successfully", nil)
-   }
-}
-
 // CreateIndex creates an ElasticSearch index with the specified mapping
-func CreateIndex(indexName string, mapping []byte) {
-	res, err := es.Indices.Create(indexName, es.Indices.Create.WithBody(bytes.NewReader(mapping)))
+func (client *Client) CreateIndex(indexName string, mapping []byte) {
+   esClient := client.esClient
+	res, err := esClient.Indices.Create(indexName, esClient.Indices.Create.WithBody(bytes.NewReader(mapping)))
 	if err != nil {
       logger.Fatal("Error creating index", map[string]any{
          "index": indexName,
@@ -43,7 +22,7 @@ func CreateIndex(indexName string, mapping []byte) {
 	}
 	defer res.Body.Close()
 	if res.IsError() {
-      logger.Fatal("Error response from ElasticSearch", map[string]any{
+      logger.Error("Error response from ElasticSearch", map[string]any{
          "error": res.String(),
       })
 	} else {
@@ -54,7 +33,8 @@ func CreateIndex(indexName string, mapping []byte) {
 }
 
 // SendToIndex sends a document to the specified index in ElasticSearch
-func SendToIndex(indexName string, document interface{}) error {
+func (client *Client) SendToIndex(indexName string, document interface{}) error {
+   esClient := client.esClient
 	// Marshal the document into JSON
 	payloadJSON, err := json.Marshal(document)
 	if err != nil {
@@ -62,10 +42,10 @@ func SendToIndex(indexName string, document interface{}) error {
 	}
 
 	// Send the document to the ElasticSearch index
-	res, err := es.Index(
+	res, err := esClient.Index(
 		indexName,
 		bytes.NewReader(payloadJSON),
-		es.Index.WithRefresh("true"), // Immediately make it available for search
+		esClient.Index.WithRefresh("true"), // Immediately make it available for search
 	)
 	if err != nil {
       return err
@@ -85,11 +65,12 @@ func SendToIndex(indexName string, document interface{}) error {
 
 // CountLogsByLevelAndSource counts logs grouped by log level and source
 // ExecuteQuery takes an ElasticSearch query as input and executes it
-func ExecuteQuery(indexName string, query string) (*esapi.Response, error) {
+func (client *Client) ExecuteQuery(indexName string, query string) (*esapi.Response, error) {
+   esClient := client.esClient
 	// Execute the search query
-	res, err := es.Search(
-		es.Search.WithIndex(indexName),
-		es.Search.WithBody(bytes.NewReader([]byte(query))),
+	res, err := esClient.Search(
+		esClient.Search.WithIndex(indexName),
+		esClient.Search.WithBody(bytes.NewReader([]byte(query))),
 	)
 	if err != nil {
       return nil, err
---
+++ internal/elasticsearch/esClient.go
@@ -0,0 +1,56 @@
+package elasticsearch
+
+import (
+	"github.com/elastic/go-elasticsearch/v9"
+
+	"github.com/khaiphan29/logpulse/pkg/logger"
+)
+
+type Client struct {
+   esClient *elasticsearch.Client
+   port string
+}
+
+var client *Client
+
+func InitClient(port string) *Client {
+   if client != nil {
+      logger.Error("Elasticsearch client is already initialized", nil)
+      return client
+   }
+
+	var err error
+	client.esClient, err = elasticsearch.NewClient(elasticsearch.Config{
+		Addresses: []string{
+			"http://localhost:" + port,
+		},
+	})
+	if err != nil {
+      logger.Fatal("Error creating Elasticsearch client", map[string]any{
+         "error": err,
+      })
+	} else {
+      logger.Info("Elasticsearch client created successfully", nil)
+   }
+
+   return client
+}
+
+func GetClient() *Client {
+   if client == nil {
+      logger.Error("Elasticsearch client is not initialized", nil)
+      return nil
+   }
+   return client
+}
+
+func (client *Client) ChangePort(port string) {
+   if client == nil {
+      logger.Error("Elasticsearch client is not initialized", nil)
+      return
+   }
+   client.port = port
+   logger.Info("Elasticsearch client port changed", map[string]any{
+      "port": port,
+   })
+}
