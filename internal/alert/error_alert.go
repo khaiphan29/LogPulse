a3501ee926eec352160a66f04337f9c5629c9ab0
Khai Phan <phantuankhai111@gmail.com>
2025-09-01 14:49:46 +0700

    update structure of pkg, ensure DI

--- internal/alert/error_alert.go
+++ internal/alert/error_alert.go
@@ -1,21 +1,47 @@
 package alert
 
 import (
-   "fmt"
-   "encoding/json"
+	"time"
 
-   "github.com/khaiphan29/logpulse/pkg/logger"
-   "github.com/khaiphan29/logpulse/internal/constants"
-   es "github.com/khaiphan29/logpulse/internal/elasticsearch"
+	"github.com/khaiphan29/logpulse/internal/constants"
+	"github.com/khaiphan29/logpulse/pkg/logger"
 )
 
-func ErrorAlert() {
+const (
+   PERIOD = "7d"
+   THRESHOLD = 2
+   CACHE_KEY = "error_alert_cache"
+   CACHE_TTL = 3600 // in seconds
+)
+
+type CacheClient interface {
+   SetRecord(key string, value any, exp int) error
+   GetRecord(key string) (string, error)
+   DeleteRecord(key string) error
+   ErrKeyNotExist() bool
+   IncreaseValue(key string, increment int64) (int64, error)
+   DecreaseValue(key string, decrement int64) (int64, error)
+}
+
+type Aggregator interface {
+   CountTotalLogsByLevel(index, level string, timegte, timelte time.Time) (int, error)
+}
+
+type ErrorAlert struct {
+   Cache CacheClient
+   Agg   Aggregator
+}
+
+var ea *ErrorAlert
+
+// trigger by Kafka consumer?
+func EvaluateError(cb func ()) {
    // Define the time interval and threshold
-   timeInterval := "7d"
-   threshold := 2
+   gte := time.Now().Add(-7 * 24 * time.Hour) // 7 days ago
+   lte := time.Now()
 
    // Call the function to get error logs by sources
-   results, err := GetErrorBySources(timeInterval, threshold)
+   results, err := ea.Agg.CountTotalLogsByLevel(constants.ES_INDEX_LOG, constants.LOG_LEVEL_ERROR, gte, lte)
    if err != nil {
       logger.Error("Error getting error logs by sources", map[string]any{
          "error": err,
@@ -23,134 +49,8 @@ func ErrorAlert() {
       return
    }
 
-   // Process the results
-   for _, result := range results {
-      for source, count := range result {
-         logger.Info("Error alert", map[string]any{
-            "source": source,
-            "count":  count,
-         })
-      }
+   if results >= THRESHOLD {
+      cb()
    }
 }
 
-func GetErrorBySources(timeInterval string, threshold int) ([]map[string]int, error) {
-	query := fmt.Sprintf(`{
-		"size": 0,
-		"query": {
-			"range": {
-				"timestamp": {
-					"gte": "now-%s",
-					"lte": "now"
-				}
-			}
-		},
-		"aggs": {
-			"by_source": {
-				"terms": {
-					"field": "source",
-					"size": 100
-				},
-				"aggs": {
-					"by_log_level": {
-						"terms": {
-							"field": "logLevel",
-							"include": "ERROR"
-						}
-					}
-				}
-			}
-		}
-	}`, timeInterval)
-
-	indexName := constants.ESIndexLogs
-
-	// Execute the query
-	res, err := es.ExecuteQuery(indexName, query)
-	if err != nil {
-		logger.Error("Error executing query", map[string]any{
-			"index": indexName,
-			"error": err,
-		})
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	// Check if the response contains errors
-	if res.IsError() {
-		logger.Error("Error response from ElasticSearch", map[string]any{
-			"index": indexName,
-			"error": res.String(),
-		})
-		return nil, fmt.Errorf("error response from ElasticSearch: %s", res.String())
-	} else {
-      logger.Info("Query executed successfully", map[string]any{
-         "index": indexName,
-         "status": res.Status(),
-         "body": res.String(),
-      })
-   }
-
-	// Decode the response
-	var result map[string]interface{}
-	if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
-		logger.Error("Error decoding response body", map[string]any{
-			"index": indexName,
-			"error": err,
-		})
-		return nil, err
-	} else {
-      logger.Info("Response decoded successfully", map[string]any{
-         "result": result,
-      })
-   }
-
-   // Extract the "aggregations" field
-   aggregations := result["aggregations"].(map[string]interface{})
-   logger.Info("Aggregations", map[string]any{
-      "aggregations": aggregations,
-   })
-
-   // Extract the "by_source" aggregation
-   bySourceAgg := aggregations["by_source"].(map[string]interface{})
-
-   // Extract the "buckets" array from the "by_source" aggregation
-   buckets := bySourceAgg["buckets"].([]interface{})
-
-	var filteredResults []map[string]int
-
-	for _, bucket := range buckets {
-		sourceBucket := bucket.(map[string]interface{})
-		source := sourceBucket["key"].(string)
-		errorLogs := 0
-
-		// Check if there are error logs for this source
-		if logLevelAgg, exists := sourceBucket["by_log_level"].(map[string]interface{}); exists {
-			for _, logLevelBucket := range logLevelAgg["buckets"].([]interface{}) {
-				logLevelData := logLevelBucket.(map[string]interface{})
-				if logLevelData["key"].(string) == "ERROR" {
-					errorLogs = int(logLevelData["doc_count"].(float64))
-				}
-			}
-		}
-
-      logger.Info("Error logs count", map[string]any{
-         "source": source,
-         "count":  errorLogs,
-      })
-
-		// Add to results if error count exceeds threshold
-		if errorLogs > threshold {
-			filteredResults = append(filteredResults, map[string]int{
-				source: errorLogs,
-			})
-		}
-	}
-
-	logger.Info("Query executed successfully", map[string]any{
-		"index": indexName,
-		"results": filteredResults,
-	})
-
-	return filteredResults, nil
-}
