5b1d6cb9b3245aee76fbe828c23332d2aa314a5f
Khai Phan <92008268+khaiphan29@users.noreply.github.com>
2025-09-06 15:51:15 +0700

    update Kafka topics config path

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -6,7 +6,7 @@ const (
 
    KAFKA_LOGS_TOPIC_KEY = "logs"
    KAFKA_LOG_RETRY_TOPIC_KEY = "logs_dlq"
-   KAFKA_TOPICS_CONFIG_PATH = "./internal/config/kafka/kafka_topics.yaml"
+   KAFKA_TOPICS_CONFIG_PATH = "./internal/config/kafka/topics.yaml"
 
    ES1_PREFIX = "ES"
    ES_LOGS_INDEX_CFG_PATH = "./internal/config/es/log_mapping.json"
