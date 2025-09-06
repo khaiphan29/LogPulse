2ec70434438a6046df8b8452f46e1ab60695adbe
Khai Phan <92008268+khaiphan29@users.noreply.github.com>
2025-09-06 15:45:21 +0700

    work with setup and config pkg

--- cmd/create_es_indexes/main.go
+++ cmd/create_es_indexes/main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
-   "github.com/khaiphan29/logpulse/internal/config/es"
-   "github.com/khaiphan29/logpulse/pkg/logger"
-   "github.com/khaiphan29/logpulse/internal/setup"
+	"github.com/khaiphan29/logpulse/internal/config/es"
+	"github.com/khaiphan29/logpulse/internal/constants"
+	"github.com/khaiphan29/logpulse/internal/setup"
+	"github.com/khaiphan29/logpulse/pkg/logger"
 )
 
 func main() {
    // Load Elasticsearch configuration
    esService := setup.SetUpESSerive()
-   indexCfg := esconfig.LoadIndexConfig("./internal/config/es/log_mapping.json")
+   indexCfg := esconfig.LoadIndexConfig(constants.ES_LOGS_INDEX_CFG_PATH)
 
    // Create the index
-   err := esService.Index.CreateIndex(indexCfg.Name, []byte(indexCfg.Mapping))
+   err := esService.Index.CreateIndex(indexCfg.Name, indexCfg.Mapping)
    if err != nil {
       logger.Error("Failed to create index", map[string]any{
+         "index": indexCfg.Name,
+         "mapping": string(indexCfg.Mapping),
          "error": err,
       })
    } else {
